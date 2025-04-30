ad5b43ead9504dd31420d77767d8d7d877e177a7
Mahfud <abdulmahfud999@gmail.com>
2025-04-30 12:41:07 +0700

    header nav user

--- src/components/nav-user-top.tsx
+++ src/components/nav-user-top.tsx
@@ -58,8 +58,8 @@ export function NavUserTop({
           </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
+            side={isMobile ? "bottom" : "bottom"}
+            align="center"
             sideOffset={4}
           >
             <DropdownMenuLabel className="p-0 font-normal">
