441e63982badf8ee4fa658de95a0d4caa9e258a5
Mahfud <abdulmahfud999@gmail.com>
2025-07-16 16:31:43 +0700

    lanjut

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -79,7 +79,7 @@ export function NavUser() {
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
                 <AvatarImage
-                  src={user.avatar || "/images/user.png"}
+                  src={"/images/user.png"}
                   alt={user.name}
                 />
                 <AvatarFallback className="rounded-lg">
@@ -105,7 +105,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage src="/images/user.png" alt={user.name} />
                   <AvatarFallback className="rounded-lg">
                     {user.name ? user.name[0] : "U"}
                   </AvatarFallback>
